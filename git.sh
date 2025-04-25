b1facbac0ca2aa719ec5e83104a7a61d5ce4c2f2
tom-park-io <tom@krononlabs.com>
2025-04-25 10:58:29 +0900

    chore: update configuration files to the latest version

--- git.sh
+++ git.sh
@@ -1,2 +1,6 @@
 git config --local user.name kyle-park-io
 git config --local user.email andy3638@naver.com
+
+# krononlabs
+git config --local user.name tom-park-io
+git config --local user.email tom@krononlabs.com
